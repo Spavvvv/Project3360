44a4af72a835e6fe9e4c626feebaa523e8d8605a
_ Cookies _ <tiendat3157@gmail.com>
2024-10-24 22:28:01 +0700

    tesing base engine

--- Project3360_/main.cpp
+++ Project3360_/main.cpp
@@ -1,27 +1,11 @@
-#include <SFML/Graphics.hpp>
-
-
 #include <iostream>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+#include "Engine.h"
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+int main() {
+	Engine Engine;
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+	Engine.running();
 
-    return 0;
+	return 0;
 }
